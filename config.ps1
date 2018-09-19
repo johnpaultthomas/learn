9d205dcd324cffc084b9cc9586f231163dc6705b
johnpaultthomas <johnpaultthomas@yahoo.co.in>
2018-09-19 14:48:09 +1200

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -2,4 +2,4 @@ $vm_name =$args[0]
 $env_name =$args[1]
 "node_name  '$vm_name'"| add-content c:\chef\client.rb -Encoding UTF8
 [System.Environment]::SetEnvironmentVariable('APPENV',$env_name,[System.EnvironmentVariableTarget]::Machine)
-Powershell C:\opscode\chef\bin\chef-client.bat -j C:\chef\first-boot.json
+Powershell C:\opscode\chef\bin\chef-client.bat --runlist role[bootstrap]
